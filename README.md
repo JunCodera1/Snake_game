904f41977b3b15ae3f9649a2809daaba418cb967
JunCodera1 <minhtien0601200@gmail.com>
2024-06-30 15:36:22 +0700

    Creating snake

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Snake game C++
 
 - [x] Creating a blank canvas & game loop
-- [ ] Creating a food
-- [ ] Creating the Snake
+- [x] Creating a food
+- [x] Creating the Snake
 - [ ] Moving the Snake
 - [ ] Making the Snake eat the Food
 - [ ] Making the Snake grow longer
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h>
+#include <deque>
 
 using namespace std;
 
@@ -9,6 +10,19 @@ Color darkGreen = {43, 51, 24, 255};
 int cellSize = 30;
 int cellCount = 25;
 
+class Snake{
+public:
+    deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+
+    void Draw(){
+        for(unsigned i = 0; i < body.size(); i++){
+            float x = body[i].x;
+            float y = body[i].y;
+            Rectangle segment = Rectangle{x * cellSize, y * cellSize, (float)cellSize, (float)cellSize};
+            DrawRectangleRounded(segment, 0.5, 6, darkGreen);
+        }
+    }
+};
 class Food
 {
 public:
@@ -46,6 +60,7 @@ int main()
     SetTargetFPS(60);
 
     Food food = Food();
+    Snake snake = Snake();
 
     while (WindowShouldClose() == false)
     {
@@ -53,6 +68,7 @@ int main()
         // Drawing
         ClearBackground(green);
         food.Draw();
+        snake.Draw();
         EndDrawing();
     }
     CloseWindow();
